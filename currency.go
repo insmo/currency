9fee75d037f8a1eaa2279be690b7ae103fadb7d7
Simon Zimmermann <simon@insmo.com>
2015-09-07 11:27:35 +0200

    add header

--- currency.go
+++ currency.go
@@ -1,3 +1,9 @@
+// Copyright 2015 Simon Zimmermann. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+// package currency implements a currency converter
+
 package currency
 
 import (
